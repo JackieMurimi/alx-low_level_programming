527df4fa581c3d06facc5fc040aa2000b3e5d91c
JackieMurimi <murimijacque@gmail.com>
2022-12-14 14:10:03 -0800

    7-print_last_digit.c:8: warning: No description found for parameter or member int 7-print_last_digit.c:9: warning: Excess function parameter n description in print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of n
+ * @n: the number
+ * Return: Value of the last digit
+ */
+int print_last_digit(int)
+{
+
+	int lastdig = n % 10;
+
+	if (lastdig < 0)
+		lastdig *= -1;
+
+	_putchar(lastdig + '0');
+
+	return (0);
+
+}
