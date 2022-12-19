d910d81acffed345f012f2315438bec43294954d
JackieMurimi <murimijacque@gmail.com>
2022-12-19 03:09:26 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: input string
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}
+	_putchar('\n');
+}
