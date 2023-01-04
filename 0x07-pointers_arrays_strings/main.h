020654e16d4c8076b469cf0a8cd85e2668f040e1
JackieMurimi <murimijacque@gmail.com>
2023-01-04 08:06:12 -0800

    adv 1

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 #endif
